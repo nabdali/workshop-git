acd4979d88452c232f7feac3b391ecde48222d59
ali boulajine <abdelali.boulajine@antenia.fr>
2022-02-16 11:28:41 +0100

    Display env name var

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>{process.env.REACT_APP_VERCEL_ENV}</p>
         <a
           about="App-link"
           href="https://reactjs.org"
