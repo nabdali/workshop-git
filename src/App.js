12b3f06e03040c56499f77ea9adc4e5bfcb66b5a
ali boulajine <abdelali.boulajine@antenia.fr>
2022-02-17 18:10:36 +0100

    feat: learn git
    Closes #2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          {" "}
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
@@ -20,7 +21,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Git
         </a>
       </header>
     </div>
